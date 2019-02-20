d5830e963ea8d00812ec017d8201d08e109938a3
Jessica <woolard.jessica@gmail.com>
2019-02-19 21:10:08 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <LikeButton/>
       </div>
     );
+  }
+}
+
+class LikeButton extends Component {
+  state = {
+    count: 0,
+  };
+  click = ()=> {
+    this.setState(({ count})=>({ count: count + 1,
+    }))
+  };
+  render() {
+    return (
+        <button onClick={this.click}>{this.state.count}{this.state.count===1? ' like':' likes'} </button>
+    )
   }
 }
 
